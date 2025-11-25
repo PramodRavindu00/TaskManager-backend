2098a79889ddc83c76986a14f98c9033498c637a
PramodRavindu00 <pramod2000.ravindu@gmail.com>
2025-11-25 22:32:34 +0530

    add project create, update endpoint

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,34 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
+import type { CurrentUserType } from '../common/types/types';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { PaginateDto } from '../common/utils/paginate';
 
 @Controller('project')
+@UseGuards(AuthGuard)
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectService.create(createProjectDto);
+  create(
+    @Body() createProjectDto: CreateProjectDto,
+    @CurrentUser() user: CurrentUserType,
+  ) {
+    return this.projectService.create(createProjectDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.projectService.findAll();
+  findAll(paginate: PaginateDto) {
+    return this.projectService.findAll(paginate);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectService.findOne(+id);
+  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.projectService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    return this.projectService.update(+id, updateProjectDto);
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() updateProjectDto: UpdateProjectDto,
+    @CurrentUser() user: CurrentUserType,
+  ) {
+    return this.projectService.update(id, updateProjectDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectService.remove(+id);
+  remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.projectService.remove(id);
   }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,26 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { PrismaService } from '../common/prisma/prisma.service';
+import { Prisma, ProjectRole } from '@prisma/client';
+import { CurrentUserType } from '../common/types/types';
+import paginateData, { PaginateDto } from '../common/utils/paginate';
 
 @Injectable()
 export class ProjectService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createProjectDto: CreateProjectDto, user: CurrentUserType) {
+    await this.prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+      const project = await tx.project.create({
+        data: { ...createProjectDto, createdBy: user.id, updatedBy: user.id },
+      });
+
+      await tx.projectMember.create({
+        data: {
+          userId: user.id,
+          projectId: project.id,
+          role: ProjectRole.Admin,
+          createdBy: user.id,
+          updatedBy: user.id,
+        },
+      });
+    });
   }
 
-  findAll() {
-    return `This action returns all project`;
+  async findAll(paginate: PaginateDto) {
+    const pagination = paginateData(paginate);
+    const [data, count] = await Promise.all([
+      this.prisma.project.findMany({ ...pagination }),
+      this.prisma.project.count(),
+    ]);
+    return { data, count };
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} project`;
   }
 
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
+  async update(
+    id: string,
+    updateProjectDto: UpdateProjectDto,
+    user: CurrentUserType,
+  ) {
+    await this.prisma.project.update({
+      where: { id },
+      data: { ...updateProjectDto, updatedBy: user.id },
+    });
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} project`;
   }
 }
